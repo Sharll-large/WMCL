c005ea525ead081dca5b8a9098f37a30eb2dbc91
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-12-08 18:54:04 +0800

    Delete WMCL directory

--- WMCL/src/main/java/org/sharll/Tools/ExecuteShell.java
+++
@@ -1,9 +0,0 @@
-package org.sharll.Tools;
-
-import java.io.IOException;
-
-public class ExecuteShell {
-    public static void Start(String command) throws IOException {
-        Runtime.getRuntime().exec(command);
-    }
-}
